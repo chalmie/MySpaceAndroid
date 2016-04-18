76f689ae1598c2ca097b40550293f4a937b16843
chalmie <blairchalmers@gmail.com>
2016-04-18 14:53:08 -0700

    add unit test to show MainActivity content with button text as submit

---
+++ app/src/test/java/com/epicodus/myspace/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.epicodus.myspace;
+
+import android.os.Build;
+import android.widget.Button;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Guest on 4/18/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextOnButton() {
+        Button profileAccessButton = (Button) activity.findViewById(R.id.profileAccessButton);
+        assertTrue("Submit".equals(profileAccessButton.getText().toString()));
+    }
+}
